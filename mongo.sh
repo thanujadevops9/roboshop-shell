0436e1b72ce18e17e54df6c01c794d9ea28f5e25
thanu <thanujadondeti102@gmail.com>
2022-11-19 11:31:49 +0530

    mongod script

---
+++ mongo.sh
@@ -0,0 +1,13 @@
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
+yum install -y mongodb-org
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+systemctl enable mongod
+systemctl restart mongod
