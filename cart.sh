5f93b0496cce6d71b6b20f943de6733ad0b05d55
thanu <thanujadondeti102@gmail.com>
2022-11-20 11:25:36 +0530

    cart script

---
+++ cart.sh
@@ -0,0 +1,14 @@
+yum install nodejs make gcc-c++ -y
+useradd roboshop
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf cart
+unzip -o  /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+#mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+#systemctl daemon-reload
+#systemctl start cart
+#systemctl enable cart
