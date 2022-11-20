ad77097afdcdde65e1614c239a42201a4f2d7ed9
thanu <thanujadondeti102@gmail.com>
2022-11-20 11:34:47 +0530

    cart script

--- cart.sh
+++ cart.sh
@@ -8,7 +8,9 @@ mv cart-main cart
 cd cart
 npm install
 
-#mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
-#systemctl daemon-reload
-#systemctl start cart
-#systemctl enable cart
+sed -i -e 's/REDIS_ENDPOINT/redis.thanujadevops9.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.thanujadevops9.online/'/home/roboshop/cart/systemd.service
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
