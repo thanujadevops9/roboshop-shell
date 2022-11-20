9dcd90b15755e1428b283966e05908ea5091b241
thanu <thanujadondeti102@gmail.com>
2022-11-20 11:20:13 +0530

    user script

--- user.sh
+++ user.sh
@@ -8,3 +8,9 @@ unzip -o /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
+
+sed -i -e 's/REDIS_ENDPOINT/redis.thanujadevops9.online/' -e 's/MONGO_ENDPOINT/mongo.thanujadevops9.online/' systemd.service
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl restart user
+systemctl enable user
