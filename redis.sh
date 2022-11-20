df2bddfb253720bd5bcf930ae6fa446c4649c400
thanu <thanujadondeti102@gmail.com>
2022-11-20 09:46:52 +0530

    redis script

--- redis.sh
+++ redis.sh
@@ -3,7 +3,9 @@ yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 yum-config-manager --enable remi
 yum install redis -y
 
+
 sed -i -e 's/127.0.0.1/0.0.0/'/etc/redis.conf /etc/redis/redis.conf
+
 
 systemctl enable redis
 systemctl restart redis
